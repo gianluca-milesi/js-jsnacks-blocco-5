bd5ccb8f2c6facd932ae84d98030bf4e1ab5a16e
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-04 22:16:39 +0100

    ADD: Snaks that were not committed

---
+++ snack-6/script.js
@@ -0,0 +1,28 @@
+const zucchine = [
+  { type: 'Napoletana', weight: 10, length: 4 },
+  { type: 'Trombetta', weight: 13, length: 16 },
+  { type: 'Napoletana', weight: 4, length: 23 },
+  { type: 'Trombetta', weight: 11, length: 6 },
+  { type: 'Napoletana', weight: 2, length: 17 },
+  { type: 'Romana', weight: 5, length: 10 },
+  { type: 'Romana', weight: 7, length: 9 },
+  { type: 'Trombetta', weight: 3, length: 8 },
+  { type: 'Calabrese', weight: 6, length: 27 },
+  { type: 'Calabrese', weight: 14, length: 4 },
+];
+
+// A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const zucchineLunghe = [];
+const zucchineCorte = [];
+
+zucchine.forEach((zucchina) => {
+  if (zucchina.length >= 15) {
+    zucchineLunghe.push(zucchina);
+  } else {
+    zucchineCorte.push(zucchina);
+  }
+});
+
+console.log(zucchineLunghe);
+console.log(zucchineCorte);
---
+++ snack-7/script.js
@@ -0,0 +1,39 @@
+const students = [
+  {
+    id: 1,
+    name: 'Marco Lanci',
+    age: 32,
+    class: '3C'
+  },
+  {
+    id: 2,
+    name: 'Mario Banfi',
+    age: 34,
+    class: '4A'
+  },
+  {
+    id: 3,
+    name: 'Luigi Banzi',
+    age: 33,
+    class: '5B'
+  },
+];
+
+// Recupera i dati dello studente con id 2
+// Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+let student
+
+students.forEach((item) => {
+  if (item.id === 2) {
+    student = item;
+  }
+});
+
+console.log(student);
+
+
+//metodo find
+// const student = students.find((student) => student.id === 2);
+
+// console.log(student);
